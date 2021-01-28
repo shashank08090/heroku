b94bb6539228743889866a49909cf7df1ea0cb2e
shashank08090 <shashank08090@gmail.com>
2021-01-28 23:15:21 +0530

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 
+
 class Contact extends Component {
   render() {
     return(
