ec82d6b1b22b2d990bb431bbf252e1280a61ef1b
shashank08090 <shashank08090@gmail.com>
2021-01-28 15:23:13 +0530

    Update resume.js

--- src/components/resume.js
+++ src/components/resume.js
@@ -19,7 +19,7 @@ class Resume extends Component {
                  />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Paul Hanna</h2>
+            <h2 style={{paddingTop: '2em'}}>Shashank Singh</h2>
             <h4 style={{color: 'grey'}}>Programmer</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
